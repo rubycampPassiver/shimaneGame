789319d783644c582af89d578d57a7005e6260ee
Notsu <notsu1106@gmail.com>
2013-02-27 17:19:58 +0900

    pt clear

--- lib/title.rb
+++ lib/title.rb
@@ -1,14 +1,16 @@
 class Title
   def initialize
+		#左クリック時のみ座標を取得
 		if Input.mouseDown?(M_LBUTTON) then
 		x = Input.mousePosX
 		y = Input.mousePosY
 		end
 	#オブジェクトの読み込み
     @bg =  Image.load(File.expand_path("../../image/title.png", __FILE__))
-	@@img_start = Sprite.new(275,200, Image.load(File.expand_path("../../image/start.png", __FILE__)))
-	@@img_end = Sprite.new(275,375, Image.load(File.expand_path("../../image/end.png", __FILE__)))
-	@pt = Sprite.new(x,y, Image.load(File.expand_path("../../image/pt.png", __FILE__)))
+	@@img_start = Sprite.new(275,150, Image.load(File.expand_path("../../image/start.png", __FILE__)))
+	@@img_level = Sprite.new(275,300, Image.load(File.expand_path("../../image/level.png", __FILE__)))
+	@@img_end = Sprite.new(275,450, Image.load(File.expand_path("../../image/end.png", __FILE__)))
+	@pt = Sprite.new(x,y, Image.load(File.expand_path("../../image/pt_1.png", __FILE__)))
   end
 
   def update
@@ -24,6 +26,7 @@ class Title
 	#オブジェクトの描画
 	Window.draw(0,0,@bg)
 	@@img_start.draw
+	@@img_level.draw
 	@@img_end.draw
 	
 	self.update
