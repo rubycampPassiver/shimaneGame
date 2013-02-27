11fc0134d48e9698f439289feb429cc069107d90
Notsu <notsu1106@gmail.com>
2013-02-27 17:46:57 +0900

    add game_tottri ~game_tokyo

--- lib/game_miyagi.rb
+++ lib/game_miyagi.rb
@@ -1,8 +1,8 @@
-﻿class Game_tottori
+﻿class Game_miyagi
   def initialize
     @bg_img = Image.load("image/game_bg.png")
 	@img_shimane = Image.load("image/shimane.png")
-	@img_tottori = Image.load("image/tottori.png")
+	@img_tottori = Image.load("image/miyagi.png")
     @tiji   = Image.load("image/tiji.png")
 
   end
@@ -11,7 +11,7 @@
     Scene.set_scene(:game) if Input.keyPush?(K_SPACE)
     Window.draw(0, 0, @bg_img)
 	Window.draw(25,25, @img_shimane)
-    Window.draw(450, 25, @img_tottori)
+    Window.draw(450, 25, @img_miyagi)
 	Window.draw( 31,392,@tiji)
 	Window.draw(255,392,@tiji)
 	Window.draw(456,392,@tiji)
