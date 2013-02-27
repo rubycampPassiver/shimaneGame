060af8ea75d0930ea6182a41103fe6da86f60fc5
Notsu <notsu1106@gmail.com>
2013-02-27 16:01:12 +0900

    change title

--- lib/title.rb
+++ lib/title.rb
@@ -1,13 +1,17 @@
 class Title
   def initialize
     @bg =  Image.load(File.expand_path("../../image/title.png", __FILE__))
+	@@img_start = Sprite.new(200,175, Image.load(File.expand_path("../../image/start.png", __FILE__)))
+	@@img_end = Sprite.new(200,375, Image.load(File.expand_path("../../image/end.png", __FILE__)))
   end
 
   def play
+
     Scene.set_scene(:game) if Input.keyPush?(K_SPACE)
 	#オブジェクトの描画
 	Window.draw(0,0,@bg)
-
+	@@img_start.draw
+	@@img_end.draw
   end
 
 end
