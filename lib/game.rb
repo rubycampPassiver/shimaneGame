8a4b495415423b04bfbb9d51fa4545db1c7e7a0a
ravencoding <flow.990mz@gmail.com>
2013-02-27 14:10:05 +0900

    #14 市民を２人２人表示完了

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,16 @@
 ﻿class Game
   def initialize
     @bg_img = Image.load("image/game.png")
+    @tiji   = Image.load("image/tiji.png")
+
   end
 
   def play
     Scene.set_scene(:game) if Input.keyPush?(K_SPACE)
     Window.draw(0, 0, @bg_img)
+	Window.draw( 31,392,@tiji)
+	Window.draw(255,392,@tiji)
+	Window.draw(456,392,@tiji)
+	Window.draw(680,392,@tiji)
   end
 end
