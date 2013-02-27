86cbdba0ff1e9383110963c6c667d043a775e498
Tomohiro <all-maisen@kxa.biglobe.ne.jp>
2013-02-27 14:10:05 +0900

    レベル選択用の画像
    レベル選択時に下に表示する画像達です。

--- lib/level.rb
+++ lib/level.rb
@@ -1,10 +1,15 @@
 class Level
   def initialize
+    #背景画像
     @bg_img = Image.load("image/title.png")
+    #タイトル文
+    @title = "対戦レベル"
+    
   end
 
   def play
     Scene.set_scene(:game) if Input.keyPush?(K_SPACE)
+    
     Window.draw(0, 0, @bg_img)
   end
 end
