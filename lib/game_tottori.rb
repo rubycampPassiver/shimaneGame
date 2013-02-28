d99b40c136286c2e0b228d6a56fe0153969ec709
Notsu <SHIMANE24020012PC@SHIMANE24020012.pref.shimane.jp>
2013-02-28 13:17:26 +0900

    add game menu

--- lib/game_tottori.rb
+++ lib/game_tottori.rb
@@ -2,14 +2,38 @@
 
 class Game_tottori
 def initialize
+
+		if Input.mouseDown?(M_LBUTTON) then
+		x = Input.mousePosX
+		y = Input.mousePosY
+		end
+	@a = 0
     @bg_img = Image.load("image/game.png")
-    @tiji   = Image.load("image/tiji.png")
+    @citizen   = Image.load("image/citizen.png")
 		@item_img = Image.load("./image/tiji.png")
 		@item_img2 = Image.load("./image/win.png")
 		@item_img3 = Image.load("./image/lose.png")
 		@items1 = []
 		@items2 = []
+
+		#スプライトクラスのオブジェクトを設定（ボタン、擬似クリック用ポインタ）
+    @img_next = Sprite.new(275,250, Image.load(File.expand_path("../../image/next.png", __FILE__)))
+	@img_end = Sprite.new(275,450, Image.load(File.expand_path("../../image/end.png", __FILE__)))
+	@img_title = Sprite.new(275,350, Image.load(File.expand_path("../../image/img_title.png", __FILE__)))
+    @pt = Sprite.new(x,y, Image.load(File.expand_path("../../image/pt_1.png", __FILE__)))
   end
+
+  #擬似ポインタをクリック先に移動
+    def update(a)
+		@pt.x = Input.mousePosX
+		@pt.y = Input.mousePosY
+
+		if a == 1
+		@img_next.draw
+		@img_end.draw
+		@img_title.draw
+		end
+	end
 
 	def add_item
 		if @items1.size <= 12
@@ -40,19 +64,41 @@ def initialize
 			end
 		end	
 	end
+
+
   def play
-      Scene.set_scene(:game) if Input.keyPush?(K_SPACE)
+
+      Scene.set_scene(:title) if Input.keyPush?(K_SPACE)
 		Sprite.update(@items1)
 		Sprite.update(@items2)
       Window.draw(0, 0, @bg_img)
-		Window.draw( 31,392,@tiji)
-		Window.draw(255,392,@tiji)
-		Window.draw(456,392,@tiji)
-		Window.draw(680,392,@tiji)
+		Window.draw( 75,459,@citizen)
+		Window.draw(255,459,@citizen)
+		Window.draw(475,459,@citizen)
+		Window.draw(680,459,@citizen)
 		Sprite.draw(@items1)
 		Sprite.draw(@items2)
 		add_item
 		Sprite.clean(@items1)
 		Sprite.clean(@items2)
+#Yが押されたとき@a＝１とする
+if Input.keyDown?(K_Y)
+	@a = 1
+end
+
+
+#コンティニューとENDボタン、擬似ポインタの表示
+	self.update(@a)
+    @pt.draw
+	
+
+
+    #ボタン処理（次の画面への遷移）
+ 	if Input.mouseDown?(M_LBUTTON) then
+      Scene.set_scene(:osaka) unless @pt.check([@img_next]).empty?
+	  Scene.finish unless @pt.check(@img_end).empty?
+	  Scene.set_scene(:title) unless @pt.check([@img_title]).empty?
+    end
+
   end
 end
