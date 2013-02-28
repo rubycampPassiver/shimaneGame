1970a6c01433f4be4e23ce33d5ceded484b1e85b
Tomohiro <all-maisen@kxa.biglobe.ne.jp>
2013-03-01 04:38:15 +0900

    Timer function is added.

--- lib/game.rb
+++ lib/game.rb
@@ -9,6 +9,8 @@ class Game
   #デフォルトコンストラクタ
   def initialize(modeint)
     
+    @nextscene = :tilte
+    
     #各モードに合わせて背景を変更
     imgname = nil
     case modeint
@@ -20,47 +22,54 @@ class Game
     @start = Image.load("image/kaishi.png")
     @img_shimane = Image.load("image/shimane.png")
     @img_enemy = Image.load("image/"+imgname)
-    @tiji   = Image.load("image/tiji.png")
     @mayor = Mayor.new #市長
     @citizens = []#市民たち
     
-    @item_img = Image.load("./image/fall_item/kani.png").setColorKey([0, 255, 0])
-    @item_img2 = Image.load("./image/fall_item/yamata.png").setColorKey([0, 255, 0]) # i_okane.png, i_shijimi.pngを追加したい.
-    @item_img3 = Image.load("./image/fall_item/rakuda.png").setColorKey([0, 255, 0])
-    @items1 = []#味方のアイテム群
-    @items2 = []#敵側のアイテム群
+    #    @item_img = Image.load("./image/fall_item/kani.png").setColorKey([0, 255, 0])
+    #    @item_img2 = Image.load("./image/fall_item/yamata.png").setColorKey([0, 255, 0]) # i_okane.png, i_shijimi.pngを追加したい.
+    #    @item_img3 = Image.load("./image/fall_item/rakuda.png").setColorKey([0, 255, 0])
     @bullet = nil #弾丸
     
+    #タイマー用変数群の宣言
+    @font  = Font.new(40)
+    @count_timer = 10
+    @i = 0
+    @min = 0
+    @sec = 0
+    @click = 0
+    setup
+    
+    Item.resetpoint
+  end
+  
+  def setup
+    @items1 = []#味方のアイテム群
+    @items2 = []#敵側のアイテム群
+    w = Window.width/2
+    d = 30
+    10.times do |i|
+      @citizens << Citizen.new(rand(w-d*2)+d, true)
+      @citizens << Citizen.new(rand(w-d*2)+w+d, false)
+    end
+    x, y = Input.mousePosX, Input.mousePosY if Input.mouseDown?(M_LBUTTON) 
     @a = 0
     @start_x = 800
     @start_y = 0
-
     #スプライトクラスのオブジェクトを設定（ボタン、擬似クリック用ポインタ）
     @img_next = Sprite.new(275,250, Image.load(File.expand_path("../../image/next.png", __FILE__)))
     @img_end = Sprite.new(275,450, Image.load(File.expand_path("../../image/end.png", __FILE__)))
     @img_title = Sprite.new(275,350, Image.load(File.expand_path("../../image/img_title.png", __FILE__)))
-    x, y = Input.mousePosX, Input.mousePosY if Input.mouseDown?(M_LBUTTON) 
     @pt = Sprite.new(x,y, Image.load(File.expand_path("../../image/pt_1.png", __FILE__)))
-    @citizens << Citizen.new(40, true)
-    @citizens << Citizen.new(100, true)
-    @citizens << Citizen.new(150, true)
-    @citizens << Citizen.new(255, true)
-    @citizens << Citizen.new(460, false)
-    @citizens << Citizen.new(500, false)
-    @citizens << Citizen.new(600, false)
-    @citizens << Citizen.new(680, false)
-    
-    Item.resetpoint
-    
+    @count_timer = 5
+    @click = 0
   end
 
   # 出現アイテムを配列に追加
   def add_item
-    item_array_size = 12
+    @item_array_size = 12
     
-    if (@items1.size <= item_array_size) and (0 < (rand(100.0)+1.0).abs and (rand(100.0)+1.0).abs < 1.5)
-      x_origin = (25..375).to_a.shuffle.first
-      y_origin = 15
+    if (@items1.size <= @item_array_size) and (0 < (rand(100.0)+1.0).abs and (rand(100.0)+1.0).abs < 1.5)
+      x_origin, y_origin = (25..375).to_a.shuffle.first, 15
       
       #確率に応じたアイテム生成
       posarr = [40,40,20]
@@ -71,9 +80,7 @@ class Game
       #アイテム決定
       posarr.each do |e|  
         sum += e
-        if flag < sum then
-          break
-        end
+        break if flag < sum
         count += 1
       end
       tmp = nil
@@ -86,7 +93,7 @@ class Game
       @items1 << tmp
     end	
     
-    if (@items2.size <= item_array_size) and (0 < (rand(100.0)+1.0).abs and (rand(100.0)+1.0).abs < 1.5)
+    if (@items2.size <= @item_array_size) and (0 < (rand(100.0)+1.0).abs and (rand(100.0)+1.0).abs < 1.5)
       x_origin = (475..(Window.width - 25)).to_a.shuffle.first
       y_origin = 15
       
@@ -118,11 +125,29 @@ class Game
   def update(a)
     @pt.x, @pt.y = Input.mousePosX, Input.mousePosY
     if a == 1
-      @img_next.draw
+	    @count_timer = 0
+      @click = 0
+      unless @modeint == ALLJP
+        @img_next.draw
+      end
       @img_end.draw
       @img_title.draw
+      @a = 0
     end
     @start_x = @start_x -20 if @start_x > -800
+    
+    #タイマー
+    @min = @count_timer / 60
+    @sec = @count_timer % 60
+    @i += 1
+    if @i == 59 then 
+      @i = 0
+      @count_timer -= 1
+      @click, @a = 1, 1 if @count_timer <= 0
+    end
+    if @sec <= 9 then Window.drawFont(365, 200, "#{@min}:0#{@sec}", @font, :color => [0, 0, 0])
+    else Window.drawFont(365, 200, "#{@min}:#{@sec}", @font, :color => [0, 0, 0])
+    end
   end
 
   def play
@@ -136,6 +161,9 @@ class Game
     
     #アイテム追加
     self.add_item
+    @citizens.each do |item| 
+      @citizens.delete(item) if item.isItemTrue and (item.x < 0 or Window.width < item.x)
+    end
 
     #玉を飛ばす処理
     @bullets = [Bullet.new(@mayor.x, @mayor.y, 0.0, Window.height,0.0, Window.width/2)] if Input.mouseDown?(M_LBUTTON)
@@ -151,10 +179,11 @@ class Game
     #ボタン処理（次の画面への遷移）
     if Input.mouseDown?(M_LBUTTON) then
       # 中断メニューが出ている時のマウスクリックイベント
-      Scene.set_scene(:game) unless @pt.check([@img_next]).empty?
+      Scene.set_scene(@nextscene) unless @pt.check([@img_next]).empty?
       Scene.finish unless @pt.check(@img_end).empty?
       Scene.set_scene(:title) unless @pt.check([@img_title]).empty?
-      
+      setup if @click == 1	
+        
       # 通常のマウスクリックイベント(玉生成)
       @bullets = [Bullet.new(@mayor.x, @mayor.y, 0.0, Window.height,0.0, Window.width/2)] if Input.mouseDown?(M_LBUTTON) 
     end
--- time.rb
+++ time.rb
@@ -1,36 +1,159 @@
-#coding: shift_jis
+class Game
 
-require 'dxruby'
+  CHUGOKU = 1
+  WESTJP = 2
+  ALLJP = 3
 
-Window.width  = 800
-Window.height = 600
+  def initialize(modeint)
+    @nextscene = :tilte
+    imgname = ''
+    @modeint = modeint
+    case modeint
+    when CHUGOKU
+      imgname = 'bg_chugoku.png'
+      @nextscene = :westjp
+    when WESTJP
+      imgname = 'bg_westjp.png'
+      @nextscene = :alljp
+    when ALLJP
+      imgname = 'bg_alljp.png'
+      @nextscene = :title
+    end
 
-image = Image.load("images/game.png")
-font  = Font.new(40)
+    @bg_img = Image.load("image/game_bg.png")
+    @start = Image.load("image/kaishi.png")
+    @img_shimane = Image.load("image/shimane.png")
+    @img_enemy = Image.load("image/"+imgname)
+    @tiji   = Image.load("image/tiji.png")
+    @item_img = Image.load("./image/fall_item/kani.png").setColorKey([0, 255, 0])
+    @item_img2 = Image.load("./image/fall_item/yamata.png").setColorKey([0, 255, 0]) # i_okane.png, i_shijimi.pngを追加したい.
+    @item_img3 = Image.load("./image/fall_item/rakuda.png").setColorKey([0, 255, 0])
+    @citizen   = Image.load("image/citizen.png")
+	
+    #タイマー用変数群の宣言
+    @font  = Font.new(40)
+    @count_timer = 10
+    @i = 0
+    @min = 0
+    @sec = 0
+    @click = 0
+    setup
+  end
 
-count_timer = 180
-i = 0
-min = 0
-sec = 0
+  def add_item
+    item_array_size = 12
+    
+    if (@items1.size <= item_array_size) and (0 < (rand(100.0)+1.0).abs and (rand(100.0)+1.0).abs < 1.5)
+      x_origin, y_origin = 0+30+ rand(200),15
+      case rand(4)
+      when 3 then tmp = Crab.new(x_origin,y_origin)
+      when 2 then tmp = Camel.new(x_origin,y_origin)
+      else tmp = Orochi.new(x_origin,y_origin)
+      end
+      @items1 << tmp
+    end	
+    
+    if (@items2.size <= item_array_size) and (0 < (rand(100.0)+1.0).abs and (rand(100.0)+1.0).abs < 1.5)
+      x_origin, y_origin = Window.width/2+30+rand(200)-150,15
+      case rand(4)
+      when 3 then @items2 << Crab.new(x_origin, y_origin)
+      when 2 then @items2 << Camel.new(x_origin, y_origin)
+      else @items2 << Orochi.new(x_origin, y_origin)
+      end	
+    end	
+  end
 
+  def setup
+    @items1 = []
+    @items2 = []
 
-Window.loop do
+    if Input.mousePush?(M_LBUTTON) then
+      x = Input.mousePosX
+      y = Input.mousePosY
+    end
+    @a = 0
+    @start_x = 800
+    @start_y = 0
+    #スプライトクラスのオブジェクトを設定（ボタン、擬似クリック用ポインタ）
+    @img_next = Sprite.new(275,250, Image.load(File.expand_path("../../image/next.png", __FILE__)))
+    @img_end = Sprite.new(275,450, Image.load(File.expand_path("../../image/end.png", __FILE__)))
+    @img_title = Sprite.new(275,350, Image.load(File.expand_path("../../image/img_title.png", __FILE__)))
+    @pt = Sprite.new(x,y, Image.load(File.expand_path("../../image/pt.png", __FILE__)))
+    @count_timer = 10
+    @click = 0
+  end
 
-Window.draw(0,0,image)
+  #擬似ポインタをクリック先に移動
+  def update(a)
+    @pt.x = Input.mousePosX
+    @pt.y = Input.mousePosY
 
-min = count_timer / 60
-sec = count_timer % 60
-i += 1
-	if i == 59 then 
-		i = 0
-		count_timer -= 1
-		if count_timer < 0 then
-			break #ここはbreakでなく０秒になったときの処理を書く
-		end
-	end
-	if sec <= 9 then　　#秒数が1～9秒のときの表記が 2:9といったようになるので2:09となるようにした
-		Window.drawFont(365, 220, "#{min}:0#{sec}", font, :color => [0, 0, 0])
-	else　　　　　      #それ以外の時は普通に表示
-		Window.drawFont(365, 220, "#{min}:#{sec}", font, :color => [0, 0, 0])
-	end
+    if a == 1
+	    @count_timer = 0
+      @click = 0
+      unless @modeint == ALLJP
+        @img_next.draw
+      end
+      @img_end.draw
+      @img_title.draw
+      @a = 0
+    end
+	
+    if @start_x > -800 then
+      @start_x = @start_x -20
+    end
+
+    #タイマー
+    @min = @count_timer / 60
+    @sec = @count_timer % 60
+    @i += 1
+    if @i == 59 then 
+      @i = 0
+      @count_timer -= 1
+      if @count_timer <= 0 then
+        @click = 1
+        @a     = 1
+      end
+    end
+    if @sec <= 9 then
+      Window.drawFont(365, 200, "#{@min}:0#{@sec}", @font, :color => [0, 0, 0])
+    else
+      Window.drawFont(365, 200, "#{@min}:#{@sec}", @font, :color => [0, 0, 0])
+    end
+  end
+
+  
+  def play
+    Scene.set_scene(:game) if Input.keyPush?(K_SPACE)
+    Sprite.update(@items1)
+    Sprite.update(@items2)
+    Window.draw(0, 0, @bg_img)
+    Window.draw(25,25, @img_shimane)
+    Window.draw(450, 25, @img_enemy)
+    Window.draw( 31,456,@citizen)
+    Window.draw(255,456,@citizen)
+    Window.draw(456,456,@citizen)
+    Window.draw(680,456,@citizen)
+   	Window.draw(@start_x,@start_y,@start)
+    if @a == 0 then
+	    Sprite.draw(@items1)	
+    	Sprite.draw(@items2)
+    	self.add_item
+    	Sprite.clean(@items1)
+    	Sprite.clean(@items2)
+    end
+    #コンティニューとENDボタン、擬似ポインタの表示
+    self.update(@a)
+    @pt.draw
+    Window.draw(@start_x,@start_y,@start)
+    #ボタン処理（次の画面への遷移）
+    if Input.mousePush?(M_LBUTTON) then 
+      Scene.set_scene(@nextscene) unless @pt.check([@img_next]).empty?
+      Scene.finish unless @pt.check(@img_end).empty?
+      Scene.set_scene(:title) unless @pt.check([@img_title]).empty?
+      if @click == 1	
+        setup
+      end
+    end
+  end
 end
